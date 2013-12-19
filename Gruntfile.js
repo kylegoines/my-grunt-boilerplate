dc558b05589f992229425abffa18ad340b858c55
kyle goines <thirteenotherslikeme@gmail.com>
2013-12-19 17:13:32 -0500

    explained what this is

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+		clear: {
+			//clear terminal on any watch task. beauty.
+			files: ['**/*'], //or be more specific
+			tasks: ['clear']
+		},
+
+		compass: {
+			dist: {
+				options: {
+					require: 'susy',
+					sassDir: 'sass',
+					cssDir: 'css',
+					environment: 'production'
+				}
+			}
+		},
+
+		uglify: {
+			my_target: {
+				files: {
+					'js/build/production.min.js': ['js/build/production.js']
+					}
+				}
+			},
+
+		concat: {
+			dist: {
+				src: [
+					'js/test.js', // All JS in the libs folder
+					'js/main.js'  // This specific file
+				],
+				dest: 'js/build/production.js',
+			}
+		},
+
+		watch: {
+			all: {
+				files: 'index.html', // Change this if you are not watching index.html
+				options: {
+					livereload: true
+				}
+			},
+			css: {
+				files: '**/*.scss',
+				tasks: ['compass']
+			},
+			options: {
+				livereload: true
+			}
+		}
+
+	});
+
+	// Load these required NPM tasks:
+	grunt.loadNpmTasks('grunt-clear');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('build',['clear','concat','uglify']);
+	grunt.registerTask('c',['clear']); // This registers the watch task as the default task. If you require more tasks, create another one
+	grunt.registerTask('default',['watch']); // This registers the watch task as the default task. If you require more tasks, create another one
+
+};
--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-my-grunt-boilerplate
+#my-grunt-boilerplate
 ====================
 
-whatever flavor of grunt im into currently
+Whatever flavor of grunt im into currently.
+
+####I'll learn yeoman later...
+
+/////////////////////////////////////////
+#plugins
+
+###clear
+
+###watch
+
+###compass
+
+###uglify
+
+###concat
+////////////////////////////////////////
+
+#commands
+
+##c
+--------------------
+clear console
+
+
+##watch
+--------------------
+watch *file* to complie sass with compass
+
+
+##build
+--------------------
+concat and uglify all js files into single file, also clears console.
